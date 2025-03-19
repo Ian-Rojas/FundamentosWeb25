8fa029374344e861b5c25da43e5357b673b69e2b
IanRojasVVH <165663341+IanRojasVVH@users.noreply.github.com>
2025-03-19 09:32:02 -0300

    ok

--- Condicion/script.js
+++ Condicion/script.js
@@ -1,16 +1,17 @@
 console.log("Funcionando correctamente");
 
 function temperaturaC(){
-    let temp = 16;
+    let temp = 30;
     if (temp <= 0) {
         alert("Hace frío");
-    } else if (temp) {
+    } else if (temp >= 0 && temp <= 25) {
         alert("La temperatura es agradable");
-    }
+    } else (temp >= 25)
+    alert("Hace calor.") 
 }
 
-function Productopares(){
-    let array = [2,4,6,7,10,12];
-    let mult = array[0] * array[1] * array[2] * array[4] * array[5];
-    alert(`El arreglo es ${array.join(" - ")}:\nResultado ${mult} `)
+function nombreUsuario(){
+    let nombreUsuario = "usuario123";
+    let password = "secreto";
+    if ()
 }
