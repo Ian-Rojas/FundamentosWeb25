ecaa0c1a66df529204bdd22408efb97e915bb629
IanRojasVVH <165663341+IanRojasVVH@users.noreply.github.com>
2025-03-19 16:03:22 -0300

    ok

--- Condicion/script.js
+++ Condicion/script.js
@@ -1,7 +1,7 @@
 console.log("Funcionando correctamente");
 
 function temperaturaC(){
-    let temp = 30;
+    let temp = prompt("Ingrese la temperatura");
     if (temp <= 0) {
         alert("Hace frío");
     } else if (temp >= 0 && temp <= 25) {
@@ -11,7 +11,33 @@ function temperaturaC(){
 }
 
 function nombreUsuario(){
-    let nombreUsuario = "usuario123";
-    let password = "secreto";
-    if ()
+    let nombreUsuario = prompt("Ingrese su usuario.");
+    let password = prompt("Ingrese su contraseña.");
+    if (nombreUsuario === "usuario123" && password === "secreto") {
+        alert("Acceso Concedido")
+    }   else {
+        alert("Acceso Denegado")
+    }
+}
+
+function num() {
+    let numero = prompt("Ingrese un número");
+    if (numero > 0) {
+        alert("El numero es positivo")
+    } else if (numero < 0) {
+        alert("El numero es negativo")
+    } else {
+        alert("El numero es cero")
+    }
+}
+
+function puntajeJuego() {
+    let puntaje = prompt("Ingrese su puntaje");
+    if (puntaje > 90) {
+        alert("Excelente!")
+    } else if (puntaje >= 70 && puntaje <= 89) {
+        alert("Buen trabajo!")
+    } else {
+        alert("Necesitas mejorar.")
+    }
 }
