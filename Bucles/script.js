2cb936b2890eebb293b5cb70801b8346e51fe167
IanRojasVVH <165663341+IanRojasVVH@users.noreply.github.com>
2025-03-26 16:11:33 -0300

    ok

--- Bucles/script.js
+++ Bucles/script.js
@@ -15,3 +15,35 @@ function bucleFor() {
     }
     alert(`Los números son: ${array.join(" - ")}`);
 }
+
+function ejercicio1() {
+    let count = 1;
+    let array = [];
+    while (count <= 5) {
+        array.push(count);
+        count++;
+    }
+    alert(`Los números son: ${array.join(" - ")}`);
+}
+
+function ejercicio2() {
+    let count = 10
+    let array = [];
+    while (count >= 1) {
+        array.push(count);
+        count--;
+    }
+    alert(`Los números son: ${array.join(" - ")}`);
+}
+
+function ejercicio3() {
+    let count = 0;
+    let array = [];
+    for(let i = 1; i<=5; i++) {
+        
+    }
+}
+
+function ejercicio4() {
+
+}
