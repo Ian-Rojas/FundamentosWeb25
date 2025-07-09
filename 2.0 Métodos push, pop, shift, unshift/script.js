6602053e42c55aef14db8caa69c305449fddcc66
IanRojasVVH <165663341+IanRojasVVH@users.noreply.github.com>
2025-07-09 09:30:10 -0400

    ok

--- 2.0 Métodos push, pop, shift, unshift/script.js
+++ 2.0 Métodos push, pop, shift, unshift/script.js
@@ -9,3 +9,36 @@ function agregarElementoCompra() {
     //appendChild inserta un valor en el elemento padre "resultado"
     resultado.appendChild(item);
 }
+
+let carrito = [];
+
+function mostrarListaCompra() {
+    const lista = document.getElementById("resultado2");
+    lista.innerHTML = ""; // Limpiar antes de mostrar
+
+    for (let i = 0; i < carrito.length; i++){
+        const li = document.createElement("li")
+        li.className = "list-group-item d-flex justify-content-between align-items-center mb-2";
+        li.textContent = `${carrito[i]}`;
+        lista.appendChild(li);
+    }
+}
+
+function agregarElemento2() {
+    const input = document.getElementById("lista2");
+    const valor = input.value.trim(); // Guarda valor sin espacios extras
+
+    if (valor === "") return;
+    carrito.push(valor) // Agregar valor al final
+
+    input.value = "";
+    input.focus(); // Agrega foco hacia input
+    mostrarListaCompra()
+}
+
+function eliminarUltimoElemento() {
+    if (carrito.length > 0) {
+        carrito.pop();
+    }
+    mostrarListaCompra()
+}
